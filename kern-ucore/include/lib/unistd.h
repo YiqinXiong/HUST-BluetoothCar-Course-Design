20715dc689d69c1f522ea684d422d9f507ca3bba
YiqinXiong <543976827@qq.com>
2020-10-13 14:51:23 +0800

    add syscall-hello error about printf

--- kern-ucore/include/lib/unistd.h
+++ kern-ucore/include/lib/unistd.h
@@ -50,6 +50,7 @@
 #define SYS_dup             130
 #define SYS_pipe            140
 #define SYS_mkfifo          141
+#define SYS_hello           142
 
 #define SYS_mount           153
 #define SYS_umount			154
